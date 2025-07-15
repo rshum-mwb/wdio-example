27ff9d2ec2600bb44ac8a2b2a67dbaf1cbf0cbdf
Roland Shum <rshum@malwarebytes.com>
2025-07-15 13:42:41 +0300

    Update test.debugBuild.js

--- test/specs/debug/test.debugBuild.js
+++ test/specs/debug/test.debugBuild.js
@@ -4,10 +4,18 @@ import applePreferencesPage from '../../pageobjects/apple/apple.preferences.page
 describe('Debug', () => {
   it('Open Apple Preferences App', async () => {
     await AllureTestReporter.addTestStep('Verify UI', async () => {
+      let isMobile = await browser.isMobile;
+      console.log(`Is mobile: ${isMobile} (type: ${typeof isMobile})`);
+      let isNativeContext = await browser.isNativeContext;
+      console.log(`Is native context: ${isNativeContext} (type: ${typeof isNativeContext})`);
       await applePreferencesPage.launchApp();
       await applePreferencesPage.waitForPageDisplay();
       const isDisplayed = await applePreferencesPage.isPageDisplayed();
       expect(isDisplayed).toBe(true);
+      isMobile = await browser.isMobile;
+      console.log(`Is mobile: ${isMobile} (type: ${typeof isMobile})`);
+      isNativeContext = await browser.isNativeContext;
+      console.log(`Is native context: ${isNativeContext} (type: ${typeof isNativeContext})`);
     });
   });
 });
